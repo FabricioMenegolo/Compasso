b2beb8dda87acb30c84249d9ef9541dab9493705
Fabricio M <fmenegolo@hotmail.com>
2023-02-20 17:32:34 -0300

    criado sprint3

---
+++ Udemy/Sprint_3/Python/0_Exercicios.py
@@ -0,0 +1,27 @@
+# 1.
+# Escreva um código Python que lê do teclado o nome e a idade de um usuário e que imprima apenas
+# o ano em que ele completará 100 anos.
+# Dica: você pode ler strings digitadas no teclado utilizando a função builtin input('mensagem').
+# Lembre-se de converter números para seu respectivo tipo (int ou float) antes fazer operações aritméticas.
+
+
+# 2.
+#Escreva um código Python que verifique se três números digitados pelo usuário são pares ou ímpares.
+# Para cada número, imprima o Par: ou Ímpar: e o número correspondente.
+
+
+# 3.
+#Escreva um código Python que imprime os números pares de 0 até 20 (incluso).
+# Dica: Utilize a função range().
+
+
+# 4.
+# Escreva um código Python que imprime todos os números primos de 1 até 100.
+# Abaixo uma imagem de exemplo dos números primos entre 1 e 1000
+
+
+# 5.
+# Escreva um código Python que tem 3 variáveis dia (22), mês(10) e ano(2022) e 
+# imprime a data completa no formato a seguir:
+# Exemplo: 22/10/2022
+# Importante: É necessário formatar as variáveis como strings antes de concatená-las e imprimi-las na tela.
