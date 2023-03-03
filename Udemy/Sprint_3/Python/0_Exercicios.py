9f623eaf3dacaa9088f2d60b7455be37699df7ab
Fabricio M <fmenegolo@hotmail.com>
2023-03-03 11:02:05 -0300

    Update 0_Exercicios.py

--- Udemy/Sprint_3/Python/0_Exercicios.py
+++ Udemy/Sprint_3/Python/0_Exercicios.py
@@ -162,3 +162,211 @@ def distinct(lista):
     return lista_distinta
     
 print(distinct(exercicio_10))
+
+#
+#11
+#Escreva um programa que lê o conteúdo do arquivo texto arquivo_texto.txt e imprime o seu conteúdo.
+# Dica: leia a documentação da função
+
+with open('arquivo_texto.txt') as arquivo:
+    leitor = arquivo.read()
+    print(leitor, end='')
+
+#
+#12
+#Leia o arquivo person.json, faça o parsing e imprima seu conteúdo.
+# Dica: leia a documentação do pacote
+
+import json
+with open('person.json', 'r') as arquivo:
+    leitor = arquivo.read()
+    ret = json.loads(leitor)
+    print(ret)
+
+#
+#13
+#Implemente a função my_map(list, f) que recebe uma lista como primeiro argumento e 
+# uma função como segundo argumento. Esta função aplica a função recebida para 
+# cada elemento da lista recebida e retorna o resultado em uma nova lista.
+"""
+Teste sua função com a lista de entrada [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+e com uma função que potência de 2 para cada elemento.
+"""
+lista_input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+def my_map(array, f):
+    nova_lista=[]
+    for i in range(len(array)):
+        nova_lista.append(array[i] ** f)
+    return nova_lista
+
+print(my_map(lista_input,2))
+
+#
+#14
+# Escreva uma função que recebe um número variável de parâmetros não nomeados e um 
+# número variado de parâmetros nomeados e imprime o valor de cada parâmetro recebido.
+
+#Teste sua função com os seguintes parâmetros:
+#(1, 3, 4, 'hello', parametro_nomeado='alguma coisa', x=20)
+"""
+Para organizar o seu código e torná-lo mais legível para a correção, é necessário que 
+você utilize a declaração 'def func()' para definir sua função
+"""
+
+def func(*args,**kwargs):
+    for var in args:
+        print(var)
+    for nome, valor in kwargs.items():
+        print(valor)
+
+func(1, 3, 4, 'hello', parametro_nomeado='alguma coisa', x=20)
+
+#
+#15
+#Implemente a classe Lampada. A classe Lâmpada recebe um booleano no seu construtor,
+# Truese a lâmpada estiver ligada, False caso esteja desligada. A classe Lampada possuí os seguintes métodos:
+"""
+liga(): muda o estado da lâmpada para ligada
+
+desliga(): muda o estado da lâmpada para desligada
+
+esta_ligada(): retorna verdadeiro se a lâmpada estiver ligada, falso caso contrário
+
+Para testar sua classe:
+
+Ligue a Lampada
+
+Imprima: A lâmpada está ligada? True
+
+Desligue a Lampada
+
+Imprima: A lâmpada ainda está ligada? False
+"""
+class Lampada:
+    def __init__(self, ligada=False):
+        self.__ligada = ligada  
+
+    def desliga(self):
+        self.__ligada = False
+        self.esta_ligada()
+
+    def liga(self):
+        self.__ligada = True
+        self.esta_ligada()
+    
+    def esta_ligada(self):
+        if self.__ligada is True:
+            print(f"A lâmpada está ligada? {self.__ligada}")
+        else:
+            print(f"A lâmpada ainda está ligada? {self.__ligada}")    
+
+lamp1 = Lampada()
+
+lamp1.liga()
+
+lamp1.desliga()
+
+#
+#16
+#Escreva uma função que recebe uma string de números separados por vírgula e retorne a soma de todos eles.
+# Depois imprima a soma dos valores.
+
+#A string deve ter valor  "1,3,4,6,10,76"
+
+def soma_string(string):
+    lista = string.split(",")
+    lista_soma = []
+    for i in lista:
+        lista_soma.append(int(i))
+    return sum(lista_soma)
+        
+print(soma_string("1,3,4,6,10,76"))
+
+#
+#17
+#Escreva uma função que recebe como parâmetro uma lista e retorna 3 listas: 
+# a lista recebida dividida em 3 partes iguais. Teste sua implementação com a lista abaixo
+# lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+def uma_para_tres(lista0):
+    lista1 = []
+    lista2 = []
+    lista3 = []
+    for i, j in enumerate(lista0):
+        if i < len(lista0)//3:
+            lista1.append(j)
+        elif i < 2 * (len(lista0)//3):
+            lista2.append(j)
+        else:
+            lista3.append(j)
+    return f'{lista1} {lista2} {lista3}'
+
+print(uma_para_tres(lista))
+
+
+#
+#18
+#Dado o dicionário a seguir:
+
+speed = {'jan':47, 'feb':52, 'march':47, 'April':44, 'May':52, 'June':53, 'july':54, 'Aug':44, 'Sept':54}
+
+#Crie uma lista com todos os valores (não as chaves!) e 
+# coloque numa lista de forma que não haja valores duplicados.
+
+def valor_dic(dic):
+    dic_valor = []
+    for nome, valor in dic.items():
+        dic_valor.append(valor)
+    lista_valor = set(dic_valor)
+    return list(lista_valor)
+
+print(valor_dic(speed))
+
+#
+#19
+#Calcule o valor mínimo, valor máximo, valor médio e a mediana da lista gerada na célula abaixo:
+"""
+Obs.: Lembrem-se, para calcular a mediana a lista deve estar ordenada!
+
+import random 
+# amostra aleatoriamente 50 números do intervalo 0...500
+random_list = random.sample(range(500),50)
+
+Use as variáveis abaixo para representar cada operação matemática
+
+mediana
+media
+valor_minimo 
+valor_maximo 
+"""
+import random
+
+random_list = random.sample(range(500), 50)
+
+def mediana(random_list):
+    ordenada = sorted(random_list)
+    
+    if (len(ordenada) % 2) == 1:
+        return ordenada[len(ordenada) // 2]
+    else:
+        return ((ordenada[len(ordenada)//2] - 1) + ordenada[len(ordenada) // 2]) / 2
+
+mediana = mediana(random_list)
+media = sum(random_list)/len(random_list)
+valor_minimo = min(random_list)
+valor_maximo = max(random_list)
+
+print(f'Media: {media}, Mediana: {mediana}, Mínimo: {valor_minimo}, Máximo: {valor_maximo}')
+
+
+#
+#20
+#Imprima a lista abaixo de trás para frente.
+
+a = [1, 0, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = []
+for i in a[::-1]:
+    b.append(i)
+print(b)
